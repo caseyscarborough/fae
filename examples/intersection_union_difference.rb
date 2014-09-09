1d24cebfb9e6952a5ba4be8df79f62e947227af0
Casey Scarborough <caseyscarborough@gmail.com>
2014-09-09 10:42:41 -0400

    Update intersection/union/difference example using set operations on the FiniteAutomata directly.

--- examples/intersection_union_difference.rb
+++ examples/intersection_union_difference.rb
@@ -12,8 +12,6 @@ require_relative '../lib/fae'
 CHARACTERS = ['a', 'b']
 LANGUAGE = Fae::Language.new(CHARACTERS)
 
-states = {}
-
 def string_has_odd_number_of_the_letter_a?(string)
   string.gsub('b', '').length % 2 == 1
 end
@@ -51,13 +49,13 @@ end
 fa_1 = Fae::FiniteAutomata.new(LANGUAGE, "the language of all strings where the number of a's is odd")
 fa_2 = Fae::FiniteAutomata.new(LANGUAGE, "the language of all strings that include the substring 'bb'")
 
-# These states were determined by drawing the state diagram.
+# These states were determined by drawing the state diagram language 1.
 fa_1.add_states([
   Fae::State.new('A', { :a => 'B', :b => 'A' }, false),
   Fae::State.new('B', { :a => 'A', :b => 'B' }, true),
 ])
 
-# These states were determined by drawing the state diagram.
+# These states were determined by drawing the state diagram for language 2.
 fa_2.add_states([
   Fae::State.new('C', { :a => 'C', :b => 'D' }, false),
   Fae::State.new('D', { :a => 'C', :b => 'E' }, false),
